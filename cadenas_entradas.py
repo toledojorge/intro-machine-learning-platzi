1217a06da6b9475923ff00ae02c2bbb4cebd1b60
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-02-10 13:24:35 -0500

    avance

---
+++ cadenas_entradas.py
@@ -0,0 +1,23 @@
+# Variable con comillas dobles
+my = "Jorge"
+print(my)
+
+# Variable con comillas simples
+my_var = 'Jorge'
+print(my_var)
+
+# Concatenacion de texto
+otra_var = 'Yo soy '+my_var
+print(otra_var)
+
+# Concatenacion de texto con formato
+ultimo_var = f'Yo soy {my_var}'
+print(ultimo_var)
+
+# Texto por entrada del usuario
+nombre_final = input('Repita cual es su nombre: ')
+print(nombre_final)
+
+# Conversion de texto a numero
+numero = int(input('Escriba un numero: '))
+print(type(numero)) 
