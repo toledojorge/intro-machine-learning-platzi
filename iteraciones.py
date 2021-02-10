32071082b8dbac408cbf7c2143675c5018899622
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-02-10 15:49:50 -0500

    Iteracciones

---
+++ iteraciones.py
@@ -0,0 +1,17 @@
+contador = 0
+
+print('------- WHILE -------')
+while contador < 10:
+    print('El contador es '+ str(contador))
+    contador = contador + 1
+
+print('------- FOR DEFINIDO -------')
+# For Definido
+for x in range(6):
+  print(x)
+
+print('------- FOR INDEFINIDO -------')
+# For Indefinido
+frutas = ['manzana', 'pera', 'mango']
+for fruta in frutas:
+    print(fruta)
---
+++ programas_ramificados.py
@@ -0,0 +1,6 @@
+numero = int(input('Escriba un numero: '))
+
+if numero >= 5:
+    print('SI es mayor/igual a 5')
+else:
+    print('NO es mayor a 5')
