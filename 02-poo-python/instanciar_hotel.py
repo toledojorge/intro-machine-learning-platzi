0e96a7f904ace2e1d61b98999efbb34ac6db1674
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-02-12 08:44:21 -0500

    instancias hotel

---
+++ 02-poo-python/instanciar_hotel.py
@@ -0,0 +1,9 @@
+class Hotel:
+    def __init__(self, numero_maximo_de_huespedes, lugares_de_estacionamiento):
+        self.numero_maximo_de_huespedes = numero_maximo_de_huespedes
+        self.lugares_de_estacionamiento = lugares_de_estacionamiento
+        self.huespedes = 0
+
+
+hotel = Hotel(numero_maximo_de_huespedes=50, lugares_de_estacionamiento=20)
+print(hotel.lugares_de_estacionamiento)
