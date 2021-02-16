20ecc712d7a3e008338d9929d365876d6def457e
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-02-16 18:46:20 -0500

    caminos

--- 03-estadistica/camino_borracho/campo.py
+++ 03-estadistica/camino_borracho/campo.py
@@ -7,4 +7,11 @@ class Campo:
         self.coordenadas_de_borrachos[borracho] = coordenada
 
     def mover_borracho(self,borracho):
-        del
+        delta_x, delta_y = borracho.camina()
+        coordenada_actual = self.coordenadas_de_borrachos[borracho]
+        nueva_coordenada = coordenada_actual.mover(delta_x,delta_y)
+
+        self.coordenadas_de_borrachos[borracho] = nueva_coordenada
+
+    def obtener_coordenada(self,borracho):
+        return self.coordenadas_de_borrachos[borracho]
