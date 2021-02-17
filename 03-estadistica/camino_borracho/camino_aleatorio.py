cb50053009e0801edcbff0c1172e32733bfa119d
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-02-17 09:05:37 -0500

    graficando

--- 03-estadistica/camino_borracho/camino_aleatorio.py
+++ 03-estadistica/camino_borracho/camino_aleatorio.py
@@ -1,6 +1,7 @@
 from borracho import BorrachoTradicional
 from campo import Campo
 from coordenada import Coordenada
+from bokeh.plotting import figure, show
 
 def caminata(campo,borracho,pasos):
     inicio = campo.obtener_coordenada(borracho)
@@ -22,18 +23,28 @@ def simular_caminata(pasos,numero_de_intentos,tipo_de_borracho):
         distancias.append(round(simulacion_caminata,1))
 
     return distancias
+
+def graficar(x,y):
+    grafica = figure(title='Camino aleatorio', x_axis_label='pasos', y_axis_label='distancia')
+    grafica.line(x,y,legend='distancia media')
+
+    show(grafica)
     
 def main(distancias_de_caminata, numero_de_intentos, tipo_de_borracho):
+    distancias_media_por_caminata = []
 
     for pasos in distancias_de_caminata:
         distancias = simular_caminata(pasos,numero_de_intentos, tipo_de_borracho)
         distancia_media = round(sum(distancias) / len(distancias),4)
         distancia_maxima = max(distancias)
         distancia_minima = min(distancias)
+        distancias_media_por_caminata.append(distancia_media)
         print(f'{tipo_de_borracho.__name__} caminata aleatoria de {pasos} pasos')
         print(f'Media = {distancia_media}')
         print(f'Max = {distancia_maxima}')
         print(f'Min = {distancia_minima}')
+    
+    graficar(distancias_de_caminata,distancias_media_por_caminata)
 
 if __name__ == '__main__':
     distancias_de_caminata = [10,100,1000,10000]
