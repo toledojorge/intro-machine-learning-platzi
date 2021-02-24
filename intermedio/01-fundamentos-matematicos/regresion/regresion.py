1367b2b6226159202eb55e1c50aa39cb2cca7d23
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-02-24 11:18:43 -0500

    order folder and module middle

---
+++ intermedio/01-fundamentos-matematicos/regresion/regresion.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+x=np.array([0.5, 0.75, 1, 1.25, 1.5, 1.75, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 4, 4.25, 4.5, 4.75, 5, 5.5]).reshape(-1,1)
+y=np.array([0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1])
+
+regresion_logistica=LogisticRegression()
+regresion_logistica.fit(x,y)
+x_nuevo = np.array([1,2,3,4,5,6]).reshape(-1,1)
+prediccion = regresion_logistica.predict(x_nuevo)
+probabilidades_prediccion = regresion_logistica.predict_proba(x_nuevo)
+
+print(probabilidades_prediccion)
+print(probabilidades_prediccion[:,1])
